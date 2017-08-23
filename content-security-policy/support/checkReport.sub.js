799d521615d93cf689a1cef288b3fe3e616cfeed
andypaicu@chromium.org <andypaicu@chromium.org>
2017-08-23 09:15:48 -0700

    Implemented the report-to functionality for webkit-residing csp
    The reporting api can now be used to send csp reports
    Did not yet implement the content csp version of this change.
    spec: https://wicg.github.io/reporting/
    
    Bug: 726634
    Change-Id: Icd5cc5699d31d0300e2bcfc6f72b636e855679ea
    Reviewed-on: https://chromium-review.googlesource.com/629083
    Commit-Queue: Andy Paicu <andypaicu@chromium.org>
    WPT-Export-Revision: 3c86f9cab497b5d9605eb859e1ef71857e1ab01b

--- content-security-policy/support/checkReport.sub.js
+++ content-security-policy/support/checkReport.sub.js
@@ -32,7 +32,9 @@
     }
   }
 
-  var timeout = document.querySelector("meta[name=timeout][content=long]") ? 50 : 5;
+  var timeout =
+      document.querySelector("meta[name=reporting-api-timeout]") ? 70 :
+          document.querySelector("meta[name=timeout][content=long]") ? 50 : 5;
   var reportLocation = location.protocol + "//" + location.host + "/content-security-policy/support/report.py?op=take&timeout=" + timeout + "&reportID=" + reportID;
 
   var reportTest = async_test("Violation report status OK.");
